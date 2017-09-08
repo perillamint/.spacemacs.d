54b73a1951767b096e811934fa1d6b3aab3abded
perillamint <perillamint@quendi.moe>
2017-09-08 11:39:55 +0900

    Add packages, rename webdev layer to vue

--- layers/vue/packages.el
+++ layers/vue/packages.el
@@ -1,4 +1,4 @@
-;;; packages.el --- webdev layer packages file for Spacemacs.
+;;; packages.el --- vue layer packages file for Spacemacs.
 ;;
 ;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
 ;;
@@ -18,22 +18,22 @@
 ;;
 ;;
 ;; Briefly, each package to be installed or configured by this layer should be
-;; added to `webdev-packages'. Then, for each package PACKAGE:
+;; added to `vue-packages'. Then, for each package PACKAGE:
 ;;
 ;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `webdev/init-PACKAGE' to load and initialize the package.
+;;   function `vue/init-PACKAGE' to load and initialize the package.
 
 ;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `webdev/pre-init-PACKAGE' and/or
-;;   `webdev/post-init-PACKAGE' to customize the package as it is loaded.
+;;   define the functions `vue/pre-init-PACKAGE' and/or
+;;   `vue/post-init-PACKAGE' to customize the package as it is loaded.
 
 ;;; Code:
 
-(defconst webdev-packages
+(defconst vue-packages
   '((vue-mode :location (recipe
                          :fetcher github
                          :repo "codefalling/vue-mode")))
-  "The list of Lisp packages required by the webdev layer.
+  "The list of Lisp packages required by the vue layer.
 
 Each entry is either:
 
@@ -60,7 +60,7 @@ Each entry is either:
       - A list beginning with the symbol `recipe' is a melpa
         recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
 
-(defun webdev/init-vue-mode ()
+(defun vue/init-vue-mode ()
   (use-package vue-mode
     :config
     ;; 0, 1, or 2, representing (respectively) none, low, and high coloring
