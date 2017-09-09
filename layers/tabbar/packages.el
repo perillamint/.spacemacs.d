b6a21c67a074880348fa7edafb4c36959739308a
perillamint <perillamint@gentoo.moe>
2017-09-09 23:12:48 +0900

    Custom tabbar group config

--- layers/tabbar/packages.el
+++ layers/tabbar/packages.el
@@ -36,55 +36,72 @@ which require an initialization must be listed explicitly in the list.")
     :init
 
     (tabbar-mode 1)
-(set-face-attribute
- 'tabbar-default nil
- :background "gray20"
- :foreground "gray20"
- :box '(:line-width 1 :color "gray20" :style nil))
-(set-face-attribute
- 'tabbar-unselected nil
- :background "gray30"
- :foreground "white"
- :box '(:line-width 5 :color "gray30" :style nil))
-(set-face-attribute
- 'tabbar-selected nil
- :background "gray75"
- :foreground "black"
- :box '(:line-width 5 :color "gray75" :style nil))
-(set-face-attribute
- 'tabbar-highlight nil
- :background "white"
- :foreground "black"
- :underline nil
- :box '(:line-width 5 :color "white" :style nil))
-(set-face-attribute
- 'tabbar-button nil
- :box '(:line-width 1 :color "gray20" :style nil))
-(set-face-attribute
- 'tabbar-separator nil
- :background "gray20"
- :height 0.6)
+    (set-face-attribute
+     'tabbar-default nil
+     :background "gray20"
+     :foreground "gray20"
+     :box '(:line-width 1 :color "gray20" :style nil))
+    (set-face-attribute
+     'tabbar-unselected nil
+     :background "gray30"
+     :foreground "white"
+     :box '(:line-width 5 :color "gray30" :style nil))
+    (set-face-attribute
+     'tabbar-selected nil
+     :background "gray75"
+     :foreground "black"
+     :box '(:line-width 5 :color "gray75" :style nil))
+    (set-face-attribute
+     'tabbar-highlight nil
+     :background "white"
+     :foreground "black"
+     :underline nil
+     :box '(:line-width 5 :color "white" :style nil))
+    (set-face-attribute
+     'tabbar-button nil
+     :box '(:line-width 1 :color "gray20" :style nil))
+    (set-face-attribute
+     'tabbar-separator nil
+     :background "gray20"
+     :height 0.6)
 
-;; Change padding of the tabs
-;; we also need to set separator to avoid overlapping tabs by highlighted tabs
-(custom-set-variables
- '(tabbar-separator (quote (0.5))))
-;; adding spaces
-(defun tabbar-buffer-tab-label (tab)
-  "Return a label for TAB.
+    ;; Change padding of the tabs
+    ;; we also need to set separator to avoid overlapping tabs by highlighted tabs
+    (custom-set-variables
+     '(tabbar-separator (quote (0.5))))
+    ;; adding spaces
+    (defun tabbar-buffer-tab-label (tab)
+      "Return a label for TAB.
 That is, a string used to represent it on the tab bar."
-  (let ((label  (if tabbar--buffer-show-groups
-                    (format "[%s]  " (tabbar-tab-tabset tab))
-                  (format "%s  " (tabbar-tab-value tab)))))
-    ;; Unless the tab bar auto scrolls to keep the selected tab
-    ;; visible, shorten the tab label to keep as many tabs as possible
-    ;; in the visible area of the tab bar.
-    (if tabbar-auto-scroll-flag
-        label
-      (tabbar-shorten
-       label (max 1 (/ (window-width)
-                       (length (tabbar-view
-                                (tabbar-current-tabset)))))))))
+      (let ((label  (if tabbar--buffer-show-groups
+                        (format "[%s]  " (tabbar-tab-tabset tab))
+                      (format "%s  " (tabbar-tab-value tab)))))
+        ;; Unless the tab bar auto scrolls to keep the selected tab
+        ;; visible, shorten the tab label to keep as many tabs as possible
+        ;; in the visible area of the tab bar.
+        (if tabbar-auto-scroll-flag
+            label
+          (tabbar-shorten
+           label (max 1 (/ (window-width)
+                           (length (tabbar-view
+                                    (tabbar-current-tabset)))))))))
 
+    ;; Tabbar grouping
+    (defun tabbar-buffer-groups ()
+      "Custom tabbar groups"
+      (list
+       (cond
+        ((string-equal "*" (substring (buffer-name) 0 1)) "emacs")
+        ((eq major-mode 'emacs-lisp-byte-code-mode) "emacs")
+        ((eq major-mode 'dired-mode) "dired")
+        ((eq major-mode 'html-mode) "web")
+        ((eq major-mode 'vue-mode) "web")
+        ((eq major-mode 'vue-html-mode) "web")
+        ((eq major-mode 'javascript-mode) "web")
+        ((eq major-mode 'js-mode) "web")
+        ((eq major-mode 'js2-mode) "web")
+        ((eq major-mode 'rust-mode) "rust")
+        (t "others")))
+      )
     )
-)
+  )
