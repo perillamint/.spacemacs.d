58f26ccfbf47e6d2f079eda3ced537e647dc4693
perillamint <perillamint@gentoo.moe>
2017-09-10 15:08:49 +0900

    Move config to seperate file, enable more layers

--- init.el
+++ init.el
@@ -43,7 +43,6 @@ values."
      helm
      auto-completion
      ;; better-defaults
-     emacs-lisp
      git
      markdown
      org
@@ -51,8 +50,9 @@ values."
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
      ;; spell-checking
-     ;; syntax-checking
-     ;; version-control
+     syntax-checking
+     version-control
+     emacs-lisp
      python
      lua
      html
@@ -60,8 +60,10 @@ values."
      bibtex
      latex
      elixir
+     erlang
      typescript
      javascript
+     yaml
      vue
      )
    ;; List of additional packages that will be installed without being
@@ -315,6 +317,12 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (setq custom-file "~/.spacemacs.d/custom.el")
+  (unless (file-exists-p custom-file)
+    (write-region ";; Do not write anything past this comment. This is where Emacs will
+;; auto-generate custom variable definitions.
+" nil custom-file))
+  (load custom-file)
   )
 
 (defun dotspacemacs/user-config ()
@@ -325,21 +333,3 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
   )
-
-;; Do not write anything past this comment. This is where Emacs will
-;; auto-generate custom variable definitions.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (tabbar org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-download markdown-toc markdown-mode htmlize helm-company helm-c-yasnippet gnuplot gh-md fuzzy company-web web-completion-data company-tern tern company-statistics company-shell company-auctex company-anaconda auto-yasnippet ac-ispell auto-complete color-theme open-color-theme smeargle orgit magit-gitflow helm-gitignore gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link evil-magit magit magit-popup git-commit with-editor yapfify web-mode tagedit slim-mode scss-mode sass-mode pyvenv pytest pyenv-mode py-isort pug-mode pip-requirements lua-mode live-py-mode less-css-mode insert-shebang hy-mode dash-functional helm-pydoc helm-css-scss haml-mode fish-mode emmet-mode cython-mode anaconda-mode pythonic tide typescript-mode org-ref pdf-tools key-chord ivy tablist ob-elixir helm-bibtex parsebib flycheck-mix flycheck-credo flycheck biblio biblio-core auctex-latexmk auctex alchemist company elixir-mode web-beautify livid-mode skewer-mode simple-httpd json-mode json-snatcher json-reformat js2-refactor yasnippet multiple-cursors js2-mode js-doc coffee-mode vue-mode ssass-mode vue-html-mode mmm-mode ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint info+ indent-guide hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile pkg-info epl helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight elisp-slime-nav dumb-jump f dash s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async)))
- '(tabbar-separator (quote (0.5))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
