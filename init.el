1634c6ec6099892f87524f190859a950fe95b46c
perillamint <perillamint@quendi.moe>
2018-03-07 16:26:39 +0900

    Add nginx layer

--- init.el
+++ init.el
@@ -67,6 +67,7 @@ This function should only modify configuration layer settings."
      javascript
      yaml
      vue
+     nginx
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
